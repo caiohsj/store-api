c4a9cbf27a54d24628b9c0e4d602f3509c3a600c
Bianca Barreto <biancabarreto@jera.com.br>
2020-04-08 19:13:41 -0400

    changing rails admin filter timezone

---
+++ lib/rails_admin/abstract_model.rb
@@ -0,0 +1,18 @@
+module RailsAdmin
+  class AbstractModel::StatementBuilder
+
+    def build_statement_for_datetime_or_timestamp
+      start_date, end_date = get_filtering_duration
+
+      start_date = current_timezone_to_utc(start_date.try(:beginning_of_day)) if start_date
+      end_date = current_timezone_to_utc(end_date.try(:end_of_day)) if end_date
+
+      range_filter(start_date, end_date)
+    end
+
+    def current_timezone_to_utc(date)
+      ActiveSupport::TimeZone.new('Brasilia').local_to_utc(date) unless date.nil?
+    end
+
+  end
+end
