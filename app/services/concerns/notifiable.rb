72a31a9702a3822b5dd71f231b3781f13969add9
Vinicius da Silva <vssilva233@gmail.com>
2020-05-18 17:09:15 -0400

    [UPDATE] add kind event

--- app/services/concerns/notifiable.rb
+++ app/services/concerns/notifiable.rb
@@ -2,24 +2,26 @@ module Concerns::Notifiable
   extend ActiveSupport::Concern
 
   def notify_fail(message: nil, params: nil, resource: nil, errors: [])
-    notify({message: message, params: params, resource: resource, errors: errors})
+    notify({message: message, params: params, resource: resource, errors: errors}, :excepetion)
   end
 
   def notify_event(message: nil, params: nil, resource: nil, extra_infos: [])
-    notify({message: message, params: params, resource: resource, extra_infos: extra_infos})
+    notify({message: message, params: params, resource: resource, extra_infos: extra_infos}, :event)
   end
 
   private
-    def notify(params)
+    def notify(params, kind)
       begin
         Raven.extra_context(infos: params)
-        Raven.capture_exception(params[:message])
+        if kind.eql?(:event)
+          Raven.send_event(params)
+        else
+          Raven.capture_exception(params[:message])
+        end
       rescue StandardError => e
         puts e.message
         puts e.backtrace.inspect
       end
     end
-
-
 end
 
