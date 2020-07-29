8de2179f6ebf6f53ba5a1ebc143278abeaee1696
Victor da Costa <victorcosta@jera.com.br>
2020-07-29 11:49:31 -0400

    [UPDATE] Change carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,14 +5,13 @@ CarrierWave.configure do |config|
     config.asset_host = "http://localhost:3000"
   else
     config.storage    = :aws
-    config.aws_bucket = Rails.application.credentials[Rails.env.to_sym][:aws_s3_bucket_name] || ''
+    config.aws_bucket = Rails.application.credentials.dig(:aws_s3_bucket_name) || ''
     config.aws_acl    = 'public-read'
 
    config.aws_credentials = {
-      access_key_id:     Rails.application.credentials[Rails.env.to_sym][:aws_s3_access_key_id] || '',
-      secret_access_key: Rails.application.credentials[Rails.env.to_sym][:aws_s3_secret_access_key] || '',
-      region:            Rails.application.credentials[Rails.env.to_sym][:aws_s3_region] || ''
+      access_key_id:     Rails.application.credentials.dig(:aws_s3_access_key_id) || '',
+      secret_access_key: Rails.application.credentials.dig(:aws_s3_secret_access_key) || '',
+      region:            Rails.application.credentials.dig(:aws_s3_region) || ''
     }
   end
 end
-
