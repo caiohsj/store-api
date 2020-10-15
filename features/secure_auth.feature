241fb9a4644cd91fa2898ce3fc5cb58b282aa44b
Victor da Costa <victorcosta@jera.com.br>
2020-10-15 13:02:31 -0400

    [UPDATE] Add new scenarios to end 2 end tests

--- features/secure_auth.feature
+++ features/secure_auth.feature
@@ -11,3 +11,9 @@ Feature: Usuarios conseguirem acessar a plataforma
     Given um usuario da aplicacao
     When ele fornece as credencias invalidas
     Then ele nao deve estar autorizado a acessar a aplicacao
+
+  Scenario: Usuario tenta fazer login na aplicação
+    Given um usuario da aplicacao
+    When ele fornece as credencias validas
+    And ele clica no botão para fazer o login
+    Then o usuario deve ter um HTTP_USER_TOKEN e um HTTP_USER_EMAIL validos
--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -6,6 +6,11 @@ When("ele fornece as credencias validas") do
   @auth = { password: "secret123", email: "diogo@jera.com.br" }
 end
 
+When('ele clica no botão para fazer o login') do
+  @test_client = TestClient.new
+  @user_login_response = @test_client.login('api/v1/users/sign_in', @auth)
+end
+
 Then("ele deve estar autorizado a acessar a aplicacao") do
   auth_response = Users::SessionService.call(session_params: @auth)
   expect(auth_response.success?).to be true
@@ -18,4 +23,12 @@ end
 Then("ele nao deve estar autorizado a acessar a aplicacao") do
   auth_response = Users::SessionService.call(session_params: @invalid_auth)
   expect(auth_response.success?).to be false
+end
+
+Then('o usuario deve ter um HTTP_USER_TOKEN e um HTTP_USER_EMAIL validos') do
+  @user.reload
+  expect(@test_client.headers['HTTP_USER_TOKEN']).to be_truthy
+  expect(@test_client.headers['HTTP_USER_EMAIL']).to be_truthy
+  expect(@test_client.headers['HTTP_USER_EMAIL']).to eq(@user.email)
+  expect(@user.token_match?(@test_client.headers['HTTP_USER_TOKEN'])).to be_truthy
 end
